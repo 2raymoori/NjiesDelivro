be4b643646b04b27d78c4ab3d3f9b0c2a736e054
2raymoori <lo2raymoori@gmail.com>
2026-01-30 21:56:28 +0100

    Set up google map integration

--- App.tsx
+++ App.tsx
@@ -6,39 +6,60 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  Linking, Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const openMaps = ()=>{
+    const addrs:string = "Havelwelle 1, 14471 Potsdam"
+    const address = encodeURIComponent(addrs)
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${address}`;
+
+    Linking.openURL(url);
+  }
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <View style={{ flex: 1 }}>
+        <Text>sdfs</Text>
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={{
+            latitude: 52.521992,
+            longitude: 13.413244,
+            latitudeDelta: 0.0992,
+            longitudeDelta: 0.0421,
+          }}
+          provider={PROVIDER_GOOGLE}
+        />
+        <Pressable onPress={openMaps}>
+          <Text style={{color:"blue"}}>Berlin, Germany</Text>
+        </Pressable>
+      </View>
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
+  mapContainer: {
     flex: 1,
+
   },
 });
 
