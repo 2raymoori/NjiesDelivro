67ee3403a13618fef5e5b694c8e39b375393cc92
2raymoori <lo2raymoori@gmail.com>
2026-01-31 01:26:19 +0100

    add react native vector icons integration

--- App.tsx
+++ App.tsx
@@ -24,6 +24,13 @@ import Bags from './src/Bag.tsx';
 import OrderADd from './src/OrderAdd.tsx';
 import { NavigationContainer } from '@react-navigation/native';
 import Account from './src/Account.tsx';
+import { FontAwesome6 } from '@react-native-vector-icons/fontawesome6';
+
+import Icon from '@react-native-vector-icons/fontawesome6';
+import { Feather } from '@react-native-vector-icons/feather';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -43,10 +50,42 @@ function App() {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
 
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown:false}}>
-          <Tab.Screen name="Bag" component={Bags} />
-          <Tab.Screen name="Add Orders" component={OrderADd} />
-          <Tab.Screen name={"Account"} component={Account} />
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen
+            options={{
+              tabBarIcon: () => (
+                <Feather name="shopping-bag" size={24} color="black" />
+              ),
+            }}
+            name="Bag"
+            component={Bags}
+          />
+          <Tab.Screen
+            name="Add Orders"
+            component={OrderADd}
+            options={{
+              tabBarIcon: () => (
+                <MaterialIcons
+                  name="add-circle"
+                  size={28}
+                  color="#E53935"
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={'Account'}
+            component={Account}
+            options={{
+              tabBarIcon: () => (
+                <MaterialIcons
+                  name="person-outline"
+                  size={28}
+                  color="#E53935"
+                />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/Account.tsx
+++ src/Account.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
-
+import { FontAwesome6 } from '@react-native-vector-icons/fontawesome6';
 const Account = () => {
   return (
     <View>
       <Text>Account Screen...</Text>
+      <FontAwesome6
+        name="circle-stop"
+        color={'red'}
+        size={100}
+        iconStyle="solid"
+      />
     </View>
   );
 };
